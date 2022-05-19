1ac0535b2c7684d9557002f80d9f9e5766c100a2
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2022-05-18 20:44:05 -0500

    feat: adding routes and services to handle github api data

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Header, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import * as info from '../package.json';
 
@@ -14,5 +14,54 @@ export class AppController {
       name: info.description,
       author: info.author,
     };
+  }
+
+  @Post('api/user')
+  @Header('Content-Type', 'application/json')
+  public async getUserData(@Body('user') user: string): Promise<any> {
+    const userData = await this.appService.getUser(user);
+    return userData;
+  }
+
+  @Post('api/repos')
+  @Header('Content-Type', 'application/json')
+  public async getRepoData(@Body('user') user: string): Promise<any> {
+    const reposData = await this.appService.getRepos(user);
+    return reposData;
+  }
+
+  @Post('api/branches')
+  @Header('Content-Type', 'application/json')
+  public async getBranchData(
+    @Body('user') user: string,
+    @Body('repo') repo: string,
+  ): Promise<any> {
+    const branchesData = await this.appService.getBranches(user, repo);
+    return branchesData;
+  }
+
+  @Post('api/commits')
+  @Header('Content-Type', 'application/json')
+  public async getCommitData(
+    @Body('user') user: string,
+    @Body('repo') repo: string,
+    @Body('branch') branch: string,
+  ): Promise<any> {
+    const commitsData = await this.appService.getCommits(user, repo, branch);
+    return commitsData;
+  }
+
+  @Post('api/data')
+  @Header('Content-Type', 'application/json')
+  public async getData(
+    @Body('user') user: string,
+    @Body('repo') repo: string,
+    @Body('branch') branch: string,
+  ): Promise<any> {
+    const userData = await this.appService.getUser(user);
+    const reposData = await this.appService.getRepos(user);
+    const branchesData = await this.appService.getBranches(user, repo);
+    const commitsData = await this.appService.getCommits(user, repo, branch);
+    return { ...userData, ...reposData, ...branchesData, ...commitsData };
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,72 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { lastValueFrom, map } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private http: HttpService, private env: ConfigService) {}
+
+  getUser(user: string): Promise<any> {
+    const userUrl = this.env.get<string>('API_USERS');
+    return lastValueFrom(
+      this.http.get<any>(`${userUrl}/${user}`).pipe(
+        map((res) => res.data),
+        map((data) => ({
+          name: data.name,
+          avatar_url: data.avatar_url,
+          html_url: data.html_url,
+        })),
+      ),
+    );
+  }
+
+  getRepos(user: string): Promise<any> {
+    const userUrl = this.env.get<string>('API_USERS');
+    return lastValueFrom(
+      this.http.get<any>(`${userUrl}/${user}/repos`).pipe(
+        map((res) => res.data),
+        map((repos) => ({
+          repos: repos.map((repo) => {
+            return repo['name'];
+          }),
+        })),
+      ),
+    );
+  }
+
+  getBranches(user: string, repo: string): Promise<any> {
+    const branchUrl = this.env.get<string>('API_REPOS');
+    return lastValueFrom(
+      this.http.get<any>(`${branchUrl}/${user}/${repo}/branches`).pipe(
+        map((res) => res.data),
+        map((branches) => ({
+          branches: branches.map((branch) => {
+            return branch['name'];
+          }),
+        })),
+      ),
+    );
+  }
+
+  getCommits(user: string, repo: string, branch: string): Promise<any> {
+    const commitUrl = this.env.get<string>('API_REPOS');
+    return lastValueFrom(
+      this.http
+        .get<any>(`${commitUrl}/${user}/${repo}/commits?sha=${branch}`)
+        .pipe(
+          map((res) => res.data),
+          map((commits) => ({
+            commits: commits.map((commit) => {
+              return {
+                sha: commit['sha'].slice(0, 7),
+                author: commit['commit'].author.name,
+                date: commit['commit'].author.date,
+                message: commit['commit'].message,
+              };
+            }),
+          })),
+        ),
+    );
   }
 }
