f71fb1a414af1e3461c738f90a36811dad75e885
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2022-05-18 20:45:48 -0500

    test: adding tests for route methods

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -1,3 +1,5 @@
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,6 +9,7 @@ describe('AppController', () => {
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule, ConfigModule.forRoot()],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -17,6 +20,38 @@ describe('AppController', () => {
   describe('root', () => {
     it('serverStatus should return "Ok"', () => {
       expect(appController.getStatus().serverStatus).toBe('Ok');
+    });
+  });
+
+  describe('api/user', () => {
+    it('should return user.name', async () => {
+      const res = await appController.getUserData('wrujel');
+      expect(res.name).toBe('Wilfredo Rujel');
+    });
+  });
+
+  describe('api/repos', () => {
+    it('should return repos', async () => {
+      const res = await appController.getRepoData('wrujel');
+      expect(res.repos).not.toBeNull();
+    });
+  });
+
+  describe('api/branches', () => {
+    it('should contain branche main', async () => {
+      const res = await appController.getBranchData('wrujel', 'github-history');
+      expect(res.branches).toContain('main');
+    });
+  });
+
+  describe('api/commits', () => {
+    it('should contain Initial commit', async () => {
+      const res = await appController.getCommitData(
+        'wrujel',
+        'makeitevent',
+        'main',
+      );
+      expect(res.commits.slice(-1)[0].message).toContain('Initial commit');
     });
   });
 });
