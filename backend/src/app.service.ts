d03e0cbc9fe576c853729480d079008f220ddcdf
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2023-05-02 08:25:14 -0500

    feat(back): fix env dependency github token is not required

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,21 +1,18 @@
+/* eslint-disable prettier/prettier */
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { lastValueFrom, map } from 'rxjs';
+
+const userUrl = 'https://api.github.com/users';
+const reposUrl = 'https://api.github.com/repos';
 
 @Injectable()
 export class AppService {
-  constructor(private http: HttpService, private env: ConfigService) {}
-
-  bearerToken = this.env.get<string>('API_TOKEN');
-  header = {
-    Authorization: `Bearer ${this.bearerToken}`,
-  };
+  constructor(private http: HttpService) {}
 
   getUser(user: string): Promise<any> {
-    const userUrl = 'https://api.github.com/users';
     return lastValueFrom(
-      this.http.get<any>(`${userUrl}/${user}`, { headers: this.header }).pipe(
+      this.http.get<any>(`${userUrl}/${user}`).pipe(
         map((res) => res.data),
         map((data) => ({
           name: data.name,
@@ -27,46 +24,35 @@ export class AppService {
   }
 
   getRepos(user: string): Promise<any> {
-    const userUrl = 'https://api.github.com/users';
     return lastValueFrom(
-      this.http
-        .get<any>(`${userUrl}/${user}/repos`, { headers: this.header })
-        .pipe(
-          map((res) => res.data),
-          map((repos) => ({
-            repos: repos.map((repo) => {
-              return repo['name'];
-            }),
-          })),
-        ),
+      this.http.get<any>(`${userUrl}/${user}/repos`).pipe(
+        map((res) => res.data),
+        map((repos) => ({
+          repos: repos.map((repo) => {
+            return repo['name'];
+          }),
+        })),
+      ),
     );
   }
 
   getBranches(user: string, repo: string): Promise<any> {
-    const branchUrl = 'https://api.github.com/repos';
     return lastValueFrom(
-      this.http
-        .get<any>(`${branchUrl}/${user}/${repo}/branches`, {
-          headers: this.header,
-        })
-        .pipe(
-          map((res) => res.data),
-          map((branches) => ({
-            branches: branches.map((branch) => {
-              return branch['name'];
-            }),
-          })),
-        ),
+      this.http.get<any>(`${reposUrl}/${user}/${repo}/branches`).pipe(
+        map((res) => res.data),
+        map((branches) => ({
+          branches: branches.map((branch) => {
+            return branch['name'];
+          }),
+        })),
+      ),
     );
   }
 
   getCommits(user: string, repo: string, branch: string): Promise<any> {
-    const commitUrl = 'https://api.github.com/repos';
     return lastValueFrom(
       this.http
-        .get<any>(`${commitUrl}/${user}/${repo}/commits?sha=${branch}`, {
-          headers: this.header,
-        })
+        .get<any>(`${reposUrl}/${user}/${repo}/commits?sha=${branch}`)
         .pipe(
           map((res) => res.data),
           map((commits) => ({
